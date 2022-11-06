621c2bca2521eaa316478d87855ea63253347288
Akash-Kharat <akashkharat1339@gmail.com>
2022-11-06 22:17:49 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
+import QRCode from 'qrcode';
+import { images } from './constants';
 function App() {
+    const [url,setUrl]= useState('');
+    const [qrcode, setQrcode] = useState('')
+
+    const GenerateQRCode= () => {
+        QRCode.toDataURL(url, (err,url) => {
+            if(err) return console.error(err)
+
+            console.log(url)
+            setQrcode(url)
+        })
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+        <div className="heading">
+            <img src={images.logo} alt="logo" className='logo' />
+            <span>QR Code Generator</span>
+        </div>
+        
+        <div className='text-input'>
+           <input 
+            type="text"
+            placeholder='Type any text'
+            value={url}
+            onChange={(e)=> setUrl(e.target.value)}
+            />
+            <button onClick={GenerateQRCode}>Generate</button>
+        </div>
+        <div className='qr-out'>    
+            {qrcode &&
+                <>
+                    <img src={qrcode} alt="qr" />  
+                    <a href={qrcode} download="QRCode">
+                    <button type="button">Download</button>
+                    </a>
+                </>
+
+
+            }
+        
+        </div>
+        
+       
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/constants/images.jsx
@@ -0,0 +1,7 @@
+
+import logo from '../assets/qrlogo.svg';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  logo,
+};
